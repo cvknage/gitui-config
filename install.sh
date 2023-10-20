42139110f2ecd1b1c997a70c9b3fa2221faffc69
Christophe Knage <cvknage@gmail.com>
2023-10-20 21:40:13 +0200

    Added Vim like keymaps

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-rm -rf ~/.config/gitui/
-ln -s "$(pwd)/theme" ~/.config/gitui
+CONFIG_DIR="$HOME/.config/gitui/"
+INSTALL_DIR="$(pwd)/install/"
+
+rm -rf $CONFIG_DIR
+rm -rf $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+cp "$(pwd)/theme/"* $INSTALL_DIR
+cp "$(pwd)/keymap/"* $INSTALL_DIR
+
+ln -s $INSTALL_DIR $CONFIG_DIR
